4236fef5c8b17dd6f1ac0d0a86b50c48124547c9
Luiz Esper <luiz.hb.esper@gmail.com>
2026-08-17 21:06:55 -0300

    chore: replace template screen with hello world

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from "@react-native/new-app-screen";
-import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
 import {
-	SafeAreaProvider,
-	useSafeAreaInsets,
-} from "react-native-safe-area-context";
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 function App() {
 	const isDarkMode = useColorScheme() === "dark";
@@ -18,27 +13,19 @@ function App() {
 	return (
 		<SafeAreaProvider>
 			<StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-			<AppContent />
+			<View style={style.container}>
+				<Text> Hello Transaction App</Text>
+			</View>
 		</SafeAreaProvider>
 	);
 }
 
-function AppContent() {
-	const safeAreaInsets = useSafeAreaInsets();
-
-	return (
-		<View style={styles.container}>
-			<NewAppScreen
-				templateFileName="App.tsx"
-				safeAreaInsets={safeAreaInsets}
-			/>
-		</View>
-	);
-}
-
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: "white",
+		justifyContent: "center",
+		alignItems: "center",
 	},
 });
 
